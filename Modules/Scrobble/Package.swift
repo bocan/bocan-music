59c49ffb5935428428fa42f2a5068883519fe34f
Chris Funderburg <chris@funderburg.me>
2026-05-07 00:06:06 +0100

    chore(packages): bump swift-tools-version to 6.2 and platform target to macOS 26

--- Modules/Scrobble/Package.swift
+++ Modules/Scrobble/Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 
 import PackageDescription
 
 let package = Package(
     name: "Scrobble",
     platforms: [
-        .macOS(.v14),
+        .macOS(.v26),
     ],
     products: [
         .library(name: "Scrobble", targets: ["Scrobble"]),
--- Modules/UI/Package.swift
+++ Modules/UI/Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 
 import PackageDescription
 
 let package = Package(
     name: "UI",
     platforms: [
-        .macOS(.v15),
+        .macOS(.v26),
     ],
     products: [
         .library(name: "UI", targets: ["UI"]),
