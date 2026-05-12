d11ef155677a209555e2765c4f5b6ef89f3243f4
Chris Funderburg <chris@funderburg.me>
2026-05-12 13:45:33 +0100

    chore: fix cloud cauldron references

--- Modules/Library/Sources/Library/Lyrics/LRClibClient.swift
+++ Modules/Library/Sources/Library/Lyrics/LRClibClient.swift
@@ -36,7 +36,7 @@ public actor LRClibClient: LRClibClientProtocol {
     // MARK: - Constants
 
     private static let baseURL = URL(string: "https://lrclib.net/api/")!
-    private static let userAgent = "Bocan/1.0 (https://github.com/bocan-music; mailto:chris@cloudcauldron.io)"
+    private static let userAgent = "Bocan/1.0 (https://github.com/bocan/bocan-music; mailto:chris@funderburg.me)"
 
     // MARK: - Dependencies
 
