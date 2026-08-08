4c567da425b8558ef00ae3976f4f2262f10d9ce0
Chris Funderburg <chris@funderburg.me>
2026-08-08 23:57:51 +0100

    docs(website): host the Liminal Dial sample station playlist

--- website/.eleventy.js
+++ website/.eleventy.js
@@ -22,6 +22,7 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "static/robots.txt": "robots.txt" });
   eleventyConfig.addPassthroughCopy({ "static/screenshots": "screenshots" });
   eleventyConfig.addPassthroughCopy({ "static/appcast.xml": "appcast.xml" });
+  eleventyConfig.addPassthroughCopy({ "static/radio": "radio" });
   eleventyConfig.addPassthroughCopy({
     "static/appcast-beta.xml": "appcast-beta.xml",
   });
